6a68049e36e2ea68baeb85477cdaacd6b7a26082
Anıl Haydar Yılmaz <anilhyilmaz@gmail.com>
2020-07-22 23:44:45 +0300

    Added restart button, score and finished the game

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
 import 'dart:async';
@@ -26,96 +25,111 @@ class MyAppHome extends StatefulWidget {
 }
 
 class _MyAppHomeState extends State<MyAppHome> {
-  String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      .toLowerCase()
-      .replaceAll(",", "")
-      .replaceAll(".", "");
+  String lorem =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          .toLowerCase()
+          .replaceAll(",", "")
+          .replaceAll(".", "");
+  int typedCharLength = 0;
   int step = 0;
   int score = 0;
   int lastTypeAt;
-  void updateLastTypeAt(){
+  void updateLastTypeAt() {
     this.lastTypeAt = new DateTime.now().millisecondsSinceEpoch;
   }
-
-
-  void onType(String value){
+ void playAgain(){
+    setState(() {
+      typedCharLength = 0;
+      step = 1;
+    });
+ }
+  void onType(String value) {
     updateLastTypeAt();
     String trimmedstring = lorem.trimLeft();
-    if(trimmedstring.indexOf(value) != 0){
+    if (trimmedstring.indexOf(value) != 0) {
       setState(() {
         step = 2;
       });
+    } else {
+      typedCharLength = value.length;
     }
-     print(value);
   }
 
-  onstartclick(){
+  onstartclick() {
     setState(() {
       updateLastTypeAt();
       step++;
     });
-    Timer.periodic(new Duration(seconds:1), (timer) {
+    var timer = Timer.periodic(new Duration(seconds: 1), (timer) {
       int now = new DateTime.now().millisecondsSinceEpoch;
 
       //GAME OVER
-      if(now - lastTypeAt > 4000) step++;
       setState(() {
-        if(step == 1) score++;
+        if (step == 1 && now - lastTypeAt > 4000){
+          timer.cancel();
+          step++;
+        }
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
-
     var shown_widget;
-    if (step==0)
-      shown_widget = <Widget>[Text("WELCOME TO GAME"),
+    if (step == 0)
+      shown_widget = <Widget>[
+        Text("WELCOME TO GAME"),
         Container(
           padding: EdgeInsets.only(top: 15),
-          child: RaisedButton(child: Text("Start Game!"),
-          onPressed: onstartclick,
-      ),
+          child: RaisedButton(
+            child: Text("Start Game!"),
+            onPressed: onstartclick,
+          ),
         ),
       ];
-    else if(step == 1)
-     shown_widget = <Widget>[Text("$score"),
-      Container(
-        margin: EdgeInsets.only(left: 100),
-        height: 40,
-        child: Marquee(
-          text: lorem,
-          velocity: 125,
-          blankSpace: 20,
-          style: TextStyle(fontSize: 24, letterSpacing: 2),
-          scrollAxis: Axis.horizontal,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          startPadding: MediaQuery.of(context).size.width,
-          accelerationDuration: Duration(seconds: 10),
-          accelerationCurve: Curves.ease,
-          decelerationDuration: Duration(milliseconds: 500),
-          decelerationCurve: Curves.easeOut,
+    else if (step == 1)
+      shown_widget = <Widget>[
+        Text("$typedCharLength"),
+        Container(
+          margin: EdgeInsets.only(left: 100),
+          height: 40,
+          child: Marquee(
+            text: lorem,
+            velocity: 125,
+            blankSpace: 20,
+            style: TextStyle(fontSize: 24, letterSpacing: 2),
+            scrollAxis: Axis.horizontal,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            startPadding: MediaQuery.of(context).size.width,
+            accelerationDuration: Duration(seconds: 10),
+            accelerationCurve: Curves.ease,
+            decelerationDuration: Duration(milliseconds: 500),
+            decelerationCurve: Curves.easeOut,
+          ),
         ),
-      ),
-      Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16, top: 32),
-        child: TextField(
-          autofocus: true,
-          onChanged: onType,
-          decoration: InputDecoration(
-              border: OutlineInputBorder(),
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 32),
+          child: TextField(
+            autofocus: true,
+            onChanged: onType,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: "Yaz Bakalım"),
+          ),
+        ),
+      ];
+    else {
+      shown_widget = <Widget>[
+        Text("Game Over, Your score is $typedCharLength "),
+        RaisedButton(
+          child: Text("Play Again!"),
+          onPressed: playAgain),
 
-              labelText: "Yaz Bakalım"),
-        ),
-      ),
-    ];
-    else{
-      shown_widget = <Widget>[Text("Game Over, Your score is $score"),
       ];
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Klavye Delikanlısı!'),
+        title: Text('Klavye Delikanlım!'),
         centerTitle: true,
       ),
       body: Center(
